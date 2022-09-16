c6172e4036d53038508ed4d13b9d24bbebe768a2
Galina <pgv21@yandex.ru>
2022-09-16 10:00:16 +0300

    save3

---
+++ Seminar7_Task52/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце
+int [,] matrix = new int [3,3];
+
+for (int i=0; i<matrix.GetLength(0); i++)
+{
+    for (int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix [i,j] = new Random().Next(0,20);
+        Console.Write(matrix[i,j]+"\t");
+    }
+    Console.WriteLine();
+}
+
+ 
+
+for (int j=0; j<matrix.GetLength(1); j++)
+{
+    double Sum = 0;
+    double averageNum = 0;
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        Sum+= matrix[i,j];
+        averageNum = Sum/3;
+        averageNum = Math.Round(averageNum, 1);
+    }
+    Console.WriteLine($"Сумма элементов {j+1} столбца = {averageNum}");
+}
